0008518140abbd15457e29ff3d4f94e405af7b92
tdambrowitz <trenton.dambrowitz@haloarc.co.uk>
2023-11-07 13:59:26 +0000

    update

---
+++ Chat Page - Copy.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\Users\trenton.dambrowitz\OneDrive - Halo ARC Ltd\Desktop\AI Stuff\Halo AI Page\Audio Minutes
+streamlit run whisper.py --server.address 192.168.0.58 --server.port 8503
