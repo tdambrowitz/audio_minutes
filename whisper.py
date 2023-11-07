77482082cc1a60912fbdc7a1b440bf9048c6ea4b
tdambrowitz <trenton.dambrowitz@haloarc.co.uk>
2023-11-07 14:52:34 +0000

    Update whisper.py

--- whisper.py
+++ whisper.py
@@ -9,7 +9,7 @@ from pydub import AudioSegment
 from moviepy.editor import *
 import math
 from openai import OpenAI
-openai = OpenAI()
+client = OpenAI()
 
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
