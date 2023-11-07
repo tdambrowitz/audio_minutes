ade0170b9f780b25370b09616b6a2c9bf5c68205
tdambrowitz <trenton.dambrowitz@haloarc.co.uk>
2023-11-07 14:17:59 +0000

    Update whisper.py

--- whisper.py
+++ whisper.py
@@ -81,7 +81,10 @@ def display_page():
 
     def transcribe_audio(audio_file_path):
         with open(audio_file_path, 'rb') as audio_file:
-            transcription = client.audio.transcriptions.create("whisper-1", audio_file)
+            transcription = client.audio.transcriptions.create(
+                model="whisper-1", 
+                file=audio_file, 
+                )
         return transcription['text']
 
     def meeting_minutes(transcription):
